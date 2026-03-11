32f9d4aa6bd5b1940419c7a8b87cac8ed7d039c1
clerickesc-cmd <clerickesc@gmail.com>
2026-03-11 13:49:36 +0800

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,4 +1,4 @@
-import { initializeApp } from "firebase/app"
+/*import { initializeApp } from "firebase/app"
 import { getAuth } from "firebase/auth"
 import { getFirestore } from "firebase/firestore"
 
@@ -9,6 +9,24 @@ const firebaseConfig = {
   storageBucket: "YOUR_BUCKET",
   messagingSenderId: "YOUR_ID",
   appId: "YOUR_APP_ID"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)*/
+import { initializeApp } from "firebase/app"
+import { getAuth } from "firebase/auth"
+import { getFirestore } from "firebase/firestore"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDatGTVP_d7M-2ORCO_4KinENhlAWmm03c",
+  authDomain: "meattrace-scc.firebaseapp.com",
+  projectId: "meattrace-scc",
+  storageBucket: "meattrace-scc.firebasestorage.app",
+  messagingSenderId: "647310243368",
+  appId: "1:647310243368:web:b360c8b1d38c47ac69e71d",
+  measurementId: "G-4Q2CKPBB5B"
 }
 
 const app = initializeApp(firebaseConfig)
